01629aa3bfab7eae29746969b60ba21e99e9cede
oktarayse <ayseoktr@hotmail.com>
2023-01-17 10:34:13 +0300

    Add project files.

---
+++ ETicaret2023/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using ETicaret2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETicaret2023.Controllers
+{
+    public class HomeController : Controller
+    {
+        ETicaretEntities db =new ETicaretEntities();
+        public ActionResult Index()
+        {
+            ViewBag.Kategoriler=db.Kategoriler.ToList();
+            ViewBag.Urunler=db.Urunler.ToList();
+            
+            return View();
+        }
+        public ActionResult Kategori(int id)
+        {
+              ViewBag.Kategoriler=db.Kategoriler.ToList();
+            ViewBag.kategori=db.Kategoriler.Find(id);
+            return View(db.Urunler.Where(x=>x.KategoriID==id).ToList());
+        }
+         public ActionResult Urun(int id)
+        {
+              ViewBag.Kategoriler=db.Kategoriler.ToList();
+           
+            return View(db.Urunler.Find(id));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebAPIProject/Controllers/KategoriController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebAPIProject.Models;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+namespace WebAPIProject.Controllers
+{
+    public class KategoriController : ApiController
+    {
+        //dört metod barındırır içinde
+        //get listeleme
+        //host
+        //put update metodu
+        //delete sil metodu
+        ETicaretEntities db = new ETicaretEntities();
+
+        [HttpGet]
+        public List<Kategoriler> Get()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Kategoriler> liste = db.Kategoriler.ToList();
+           
+           
+            return liste;
+        }
+
+        [HttpGet]
+       
+        public IHttpActionResult Get(int id)
+        {
+           
+            Kategoriler kategori = db.Kategoriler.Find(id);
+            Kategori kat = new Kategori()
+            {
+                Id = kategori.KategoriID,
+                name = kategori.KategoriAdi
+            };
+
+            return Ok(kat);
+        }
+    }
+}
